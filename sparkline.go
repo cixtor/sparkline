fd918f00c539fa84b510d0b6f6e56b86c67ed772
Cixtor <cixtords@gmail.com>
2016-12-07 09:00:00 -0500

    Add support for os.Stdin to allow piped content
    Instead of receiving the list of numbers from the Terminal the program
    will start using os.Stdin to receive the numbers from the standard input
    allowing the concatenation of other commands like Curl, Cut, Grep, Sort,
    etc. Notice that this commit will also remove support for input that
    comes directly from the Terminal for simplicity.

--- sparkline.go
+++ sparkline.go
@@ -1,58 +1,19 @@
-/**
- * Sparkline
- * https://cixtor.com/
- * https://github.com/cixtor/sparkline
- * https://en.wikipedia.org/wiki/Sparkline
- * https://en.wikipedia.org/wiki/Kagi_chart
- *
- * A sparkline is a very small line chart, typically drawn without axes or
- * coordinates. It presents the general shape of the variation (typically over
- * time) in some measurement, such as temperature or stock market price, in a
- * simple and highly condensed way. Sparklines are small enough to be embedded
- * in text, or several sparklines may be grouped together as elements of a small
- * multiple.
- *
- * Whereas the typical chart is designed to show as much data as possible, and
- * is set off from the flow of text, sparklines are intended to be succinct,
- * memorable, and located where they are discussed.
- *
- * Sparklines are frequently used in line with text. For example: The Dow Jones
- * Industrial Average for February 7, 2006 sparkline which illustrates the
- * fluctuations in the Down Jones index on February 7, 2006. The sparkline
- * should be about the same height as the text around it. Tufte offers some
- * useful design principles for the sizing of sparklines to maximize their
- * readability.
- */
-
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
-func DisplayHelp() {
-	fmt.Println("Sparkline")
-	fmt.Println("  https://cixtor.com/")
-	fmt.Println("  https://github.com/cixtor/sparkline")
-	fmt.Println("  https://en.wikipedia.org/wiki/Sparkline")
-	fmt.Println("  https://en.wikipedia.org/wiki/Kagi_chart")
-	fmt.Println("Usage:")
-	fmt.Println("  spark [-h|-v] [values]")
-	fmt.Println("Examples:")
-	fmt.Println("  spark 1 5 22 13 53")
-	fmt.Println("  spark $(echo 9 13 5 17 1)")
-	fmt.Println("  spark $(seq 0 50 | sort -R)")
-}
-
-func SparkNumbers(arguments []string, verbose bool) int {
-	var numbers []float64 = GetNumbers(arguments)
+func SparkNumbers(nums []string) int {
+	var output string /* generated sparkline */
+	var numbers []float64 = GetNumbers(nums)
 	var maxNum float64 = MaxSlice(numbers)
 	var numLen int = len(numbers)
 	var sparks = make([]rune, 0)
-
-	// Unicode representation of the sparks.
 	var sticks = []rune{
 		'\u2581',
 		'\u2582',
@@ -66,41 +27,15 @@ func SparkNumbers(arguments []string, verbose bool) int {
 
 	// Get the sparkline for each number.
 	for i := 0; i < numLen; i++ {
-		var unit int = int((numbers[i] * 7) / maxNum)
+		unit := int((numbers[i] * 7) / maxNum)
 		sparks = append(sparks, sticks[unit])
 	}
 
-	if verbose {
-		var minNum float64 = MinSlice(numbers)
-		var ellipsisLimit int = numLen
-		var addThreeDots bool = false
-		var ellipsisList []string
-
-		if numLen > 15 {
-			ellipsisLimit = 15
-			addThreeDots = true
-		}
-
-		for j := 0; j < ellipsisLimit; j++ {
-			ellipsisList = append(ellipsisList, Float2String(numbers[j]))
-		}
-
-		if addThreeDots {
-			ellipsisList = append(ellipsisList, "\u2026")
-		}
-
-		fmt.Println("Sparkline")
-		fmt.Println("Quantity:", numLen)
-		fmt.Println("Minimum:", minNum)
-		fmt.Println("Maximum:", maxNum)
-		fmt.Println("Numbers:", ellipsisList)
-	}
-
 	// Print the sparklines in the console.
-	var output string
 	for i := 0; i < numLen; i++ {
 		output += string(sparks[i])
 	}
+
 	fmt.Println(output)
 
 	return 0
@@ -133,38 +68,31 @@ func MaxSlice(list []float64) float64 {
 	return max
 }
 
-func MinSlice(list []float64) float64 {
-	var min float64
-	var list_len int = len(list)
-
-	for i := 0; i < list_len; i++ {
-		var value float64 = list[i]
-
-		if value > min {
-			min = value
-		}
-	}
-
-	return min
-}
-
-func Float2String(number float64) string {
-	return strconv.FormatFloat(number, 'f', 1, 64)
-}
-
 func main() {
-	if len(os.Args) < 2 || os.Args[1] == "-h" {
-		DisplayHelp()
-		os.Exit(2)
+	var lines []string
+	var scanner *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
 	}
 
-	var arguments []string = os.Args[1:]
-	var verbose bool = false
-
-	if os.Args[1] == "-v" {
-		arguments = os.Args[2:]
-		verbose = true
+	if len(lines) == 1 {
+		/* Explode by spaces if they are concatenated */
+		lines = strings.Split(lines[0], "\x20")
 	}
 
-	os.Exit(SparkNumbers(arguments, verbose))
+	if lines == nil {
+		fmt.Println("Sparkline")
+		fmt.Println("  https://cixtor.com/")
+		fmt.Println("  https://github.com/cixtor/sparkline")
+		fmt.Println("  https://en.wikipedia.org/wiki/Sparkline")
+		fmt.Println("  https://en.wikipedia.org/wiki/Kagi_chart")
+		fmt.Println("Usage:")
+		fmt.Println("  echo 5 10 22 13 53 | sparkline")
+		fmt.Println("  seq 0 50 | sort -R | sparkline")
+		fmt.Println("  stock-market +GOOG | sparkline")
+		os.Exit(1)
+	}
+
+	os.Exit(SparkNumbers(lines))
 }
