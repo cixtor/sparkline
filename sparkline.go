4b4adcc89802306f5d840ba2cbb7254703921957
Cixtor <cixtords@gmail.com>
2016-12-08 09:00:00 -0500

    Add comments for exported functions and coding standards

--- sparkline.go
+++ sparkline.go
@@ -8,13 +8,18 @@ import (
 	"strings"
 )
 
+// SparkNumbers takes a list of numbers, converts them to floating points and
+// generates the sparkline chart from the data. There is a limited number of
+// bars provided by the Unicode standard, so there will be the case where
+// different numbers are shown in the chart as if they were in the same group.
 func SparkNumbers(nums []string) int {
 	var output string /* generated sparkline */
-	var numbers []float64 = GetNumbers(nums)
-	var maxNum float64 = MaxSlice(numbers)
-	var numLen int = len(numbers)
-	var sparks = make([]rune, 0)
-	var sticks = []rune{
+
+	numbers := GetNumbers(nums)
+	maxNum := MaxSlice(numbers)
+	numLen := len(numbers)
+	sparks := make([]rune, 0)
+	sticks := []rune{
 		'\u2581',
 		'\u2582',
 		'\u2583',
@@ -41,24 +46,36 @@ func SparkNumbers(nums []string) int {
 	return 0
 }
 
+// GetNumbers takes all the string-encoded numbers from the standard input and
+// converts them into integers. Non-valid numbers are ignored. Notice that this
+// program handles all entries as floating points by default, even if the user
+// specifies integers the program will treat them as float64.
 func GetNumbers(letters []string) []float64 {
 	var numbers []float64
-	var list_len int = len(letters)
 
-	for i := 0; i < list_len; i++ {
-		num, _ := strconv.ParseFloat(letters[i], 64)
+	listlen := len(letters)
+
+	for i := 0; i < listlen; i++ {
+		num, err := strconv.ParseFloat(letters[i], 64)
+
+		if err != nil {
+			continue
+		}
+
 		numbers = append(numbers, num)
 	}
 
 	return numbers
 }
 
+// MaxSlice returns the maximum number from a list.
 func MaxSlice(list []float64) float64 {
 	var max float64
-	var list_len int = len(list)
 
-	for i := 0; i < list_len; i++ {
-		var value float64 = list[i]
+	listlen := len(list)
+
+	for i := 0; i < listlen; i++ {
+		value := list[i]
 
 		if value > max {
 			max = value
@@ -70,7 +87,8 @@ func MaxSlice(list []float64) float64 {
 
 func main() {
 	var lines []string
-	var scanner *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
+	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
